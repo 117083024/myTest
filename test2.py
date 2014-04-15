a795f12a44a8c42d5aa6c22261e3eed4cd1cdba1
117083024 <jxz1113@live.com>
2014-04-15 21:02:24 +0800

    python
    python test

---
+++ test2.py
@@ -0,0 +1,15 @@
+﻿#-------------------------------------------------------------------------------
+# Name:        模块1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     15/04/2014
+# Copyright:   (c) Administrator 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+s=raw_input("name: ")
+print s
+raw_input("enter any ket!")
